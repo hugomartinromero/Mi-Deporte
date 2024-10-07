bfec42210347caf2728f59e7f151edd9c1e24c7c
hugomartinromero <martin.romero.hugo@iescamas.es>
2024-10-07 19:12:10 +0200

    Diseño app terminado

---
+++ app/src/main/java/com/fireboy/mideporte/MainActivity.java
@@ -0,0 +1,23 @@
+package com.fireboy.mideporte;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
